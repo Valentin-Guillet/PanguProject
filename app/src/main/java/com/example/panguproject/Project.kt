2610b3c5152ecd2bdf269641f88c52f6f5f16c90
Val95240 <guillet.valentin95240@gmail.com>
2024-03-31 23:59:17 +0200

    Add project descriptions

--- app/src/main/java/com/example/panguproject/Project.kt
+++ app/src/main/java/com/example/panguproject/Project.kt
@@ -26,42 +26,62 @@ class Project(
 val allProjectsList: List<Project> = listOf(
     Project(
         "Athena Project",
-        costFunction = ::isPair
+        costDescription = "Four wild dice.",
+        shortCostDescription = "Four wilds",
+        costFunction = Dice::isPair,
     ),
     Project(
         "Dazbog Project",
-        costFunction = ::isPair
+        costDescription = "Seven dice of a kind.",
+        shortCostDescription = "Seven of a kind",
+        costFunction = Dice::isPair,
     ),
     Project(
         "Freya Project",
-        costFunction = ::isPair
+        costDescription = "Four pairs of dice in a row.\n\tExample: 2, 2, 3, 3, 4, 4, 5, 5",
+        shortCostDescription = "Four pairs\nin a row",
+        costFunction = Dice::isPair,
     ),
     Project(
         "Herus Project",
-        costFunction = ::isPair
+        costDescription = "Six in a row (so 1, 2, 3, 4, 5, 6)\n\tThis project can't be purchased if a reroll has been used this turn.",
+        shortCostDescription = "Six in a row\n(no reroll)",
+        costFunction = Dice::isPair,
     ),
     Project(
         "Inari Project",
-        costFunction = ::isPair
+        costDescription = "Six dice of value 1.",
+        shortCostDescription = "Six 1s",
+        costFunction = Dice::isPair,
     ),
     Project(
         "Pangu Project",
-        costFunction = ::isPair
+        costDescription = "A set of dice that amounts to exactly 40.",
+        shortCostDescription = "Sum = 40",
+        costFunction = Dice::isPair,
     ),
     Project(
         "Quetzalcoatl Project",
-        costFunction = ::isPair
+        costDescription = "Ten even dice OR ten odd dice. Each stored die count as two.",
+        shortCostDescription = "10 even OR odd\n(stored count x2)",
+        costFunction = Dice::isPair,
     ),
     Project(
         "Te Kore Project",
-        costFunction = ::isPair
+        costDescription = "Two sets of four of a kind dice.",
+        shortCostDescription = "Two sets of\nfour of a kind",
+        costFunction = Dice::isPair,
     ),
     Project(
         "Vesta Project",
-        costFunction = ::isPair
+        costDescription = "Two dice triples. Can only be purchased if a building has been built this turn.",
+        shortCostDescription = "Two triples after\nbuilding a blueprint",
+        costFunction = Dice::isPair,
     ),
     Project(
         "Vishnu Project",
-        costFunction = ::isPair
+        costDescription = "Six dice of value 6.",
+        shortCostDescription = "Six 6s",
+        costFunction = Dice::isPair,
     ),
 )

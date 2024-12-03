18f6123103ff79b54a83e9eb9a7d9f334bba4b70
Valentin-Guillet <guillet.valentin95240@gmail.com>
2024-12-03 21:11:12 +0100

    Auto-detect wild dice when checking cost

--- app/src/main/java/com/vguillet/panguproject/model/Dice.kt
+++ app/src/main/java/com/vguillet/panguproject/model/Dice.kt
@@ -17,7 +17,8 @@ data class Dice(
                 if (diceList.size != number)
                     return false
 
-                return diceList.all { it.value == diceList[0].value }
+                val nonWildDice = diceList.filter { !it.wild }
+                return nonWildDice.all { it.value == nonWildDice[0].value }
             }
         }
 
@@ -26,22 +27,23 @@ data class Dice(
                 if (diceList.size != number)
                     return false
 
-                val sortedDiceList = diceList.sortedBy { it.value }
-                for (i in 0 until number - 1) {
-                    if (sortedDiceList[i].value != sortedDiceList[i + 1].value - 1)
-                        return false
-                }
-                return true
+                val nonWildDice = diceList.filter { !it.wild }
+                val diceFreq = nonWildDice.groupingBy { it.value }.eachCount()
+
+                if (diceFreq.any { it.value > 1 })
+                    return false
+                return nonWildDice.maxOf { it.value } - nonWildDice.minOf { it.value } + 1 <= number
             }
         }
 
         fun isSet(values: List<Int>): (List<Dice>) -> Boolean {
-            val sortedValues = values.sorted()
             return fun(diceList: List<Dice>): Boolean {
                 if (diceList.size != values.size)
                     return false
 
-                return (diceList.map { it.value }.sorted() == sortedValues)
+                val diceFreq = diceList.filter { !it.wild }.groupingBy { it.value }.eachCount()
+                val valFreq = values.groupingBy { it }.eachCount()
+                return diceFreq.all { it.value <= valFreq.getOrDefault(it.key, 0) }
             }
         }
 
@@ -49,17 +51,18 @@ data class Dice(
             if (diceList.size != 5)
                 return false
 
-            val sortedDiceList = diceList.sortedBy { it.value }
-            return sortedDiceList[0].value == sortedDiceList[1].value &&
-                    sortedDiceList[3].value == sortedDiceList[4].value &&
-                    sortedDiceList[0].value != sortedDiceList[4].value &&
-                    (sortedDiceList[2].value == sortedDiceList[1].value ||
-                            sortedDiceList[2].value == sortedDiceList[3].value)
+            val diceFreq =
+                diceList.filter { !it.wild }.groupingBy { it.value }.eachCount().values.sorted()
+            return diceFreq.isEmpty() || (diceFreq.size == 1 && diceFreq[0] <= 3) ||
+                    (diceFreq.size == 2 && diceFreq[0] <= 2 && diceFreq[1] <= 3)
         }
 
         fun sumsTo(value: Int): (List<Dice>) -> Boolean {
             return fun(diceList: List<Dice>): Boolean {
-                return diceList.sumOf { it.value } == value
+                val nonWildDice = diceList.filter { !it.wild }
+                val sum = nonWildDice.sumOf { it.value }
+                val nbWild = diceList.size - nonWildDice.size
+                return value - sum in nbWild..6 * nbWild
             }
         }
 
@@ -68,11 +71,10 @@ data class Dice(
                 if (diceList.size != nbKind * nbRow)
                     return false
 
-                val sortedDiceList = diceList.map { it.value }.sorted()
-                val correctValues =
-                    List(nbRow) { i -> List(nbKind) { sortedDiceList[0] + i } }.flatten()
-
-                return sortedDiceList == correctValues
+                val nonWildDice = diceList.filter { !it.wild }
+                val diceFreq = nonWildDice.groupingBy { it.value }.eachCount()
+                return nonWildDice.maxOf { it.value } - nonWildDice.minOf { it.value } + 1 <= nbRow
+                        && diceFreq.all { it.value <= nbKind }
             }
         }
 
@@ -81,8 +83,8 @@ data class Dice(
                 if (diceList.size != nbSets * nbKind)
                     return false
 
-                val freqMap = diceList.groupingBy { it.value }.eachCount()
-                return freqMap.size == nbSets && freqMap.all { it.value == nbKind }
+                val diceFreq = diceList.filter { !it.wild }.groupingBy { it.value }.eachCount()
+                return diceFreq.size <= nbSets && diceFreq.all { it.value <= nbKind }
             }
         }
     }
